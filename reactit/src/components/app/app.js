1dc8441659f16767f81cd80ba0757c4498c7a7fe
Nikolay <murzinov.nikolay@yandex.ru>
2019-07-25 19:12:41 +0300

    nnja

---
+++ reactit/src/components/app/app.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+import AppHeader from '../app-header/app-header';
+import SearchPanel from '../search-panel/search-panel';
+import TodoList from '../todo-list/todo-list';
+import ItemStatusFilter from '../item-status-filter/item-status-filter';
+import ItemAddForm from '../item-add-form'
+
+import './app.css';
+
+class App extends Component {
+
+  maxId = 100
+
+  state = {
+    todoData:[
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  }
+  deleteItem = (id) => {
+    this.setState( ({ todoData }) => {
+      const idx = todoData.findIndex( (el) => el.id === id )
+      const newArray = [ ...todoData.slice(0,idx), ...todoData.slice(idx+1) ]
+      return {
+        todoData: newArray
+      }
+    })
+  }
+  addItem = (text) => {
+   const newItem = {
+     label: text,
+     important: false,
+     id: this.maxId++
+   }
+    
+   this.setState(({todoData})=> {
+     const newArray = [  ...todoData, newItem ]
+     return {
+       todoData: newArray
+     }
+   })
+  }
+
+  render(){
+
+    const { todoData } = this.state
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={todoData} 
+        onDeleted = { this.deleteItem }/>
+        <ItemAddForm 
+        onItemAdded={this.addItem}/>
+      </div>
+    );
+  }
+   
+  
+} 
+
+export default App
